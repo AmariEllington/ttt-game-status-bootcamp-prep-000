8b66b49fd83a432c5358b80a29634a0929b508ef
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-05 20:10:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,15 @@ WIN_COMBINATIONS = [
 ]
 
 def won(board)
-  if WIN_COMBINATIONS != WIN_COMBINATIONS[0] + 1
-    return false
+if WIN_COMBINATIONS == [0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+
+return != true 
 
 end
