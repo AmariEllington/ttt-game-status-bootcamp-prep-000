e004f102278ac250f4de17399253be5ef795b671
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-05 20:21:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,14 +5,14 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6],
+  [0,1,2], # top row win
+  [3,4,5], # middle row win
+  [6,7,8], # bottom row win
+  [0,3,6], # left column win
+  [1,4,7], # middle column win
+  [2,5,8], # right column win
+  [0,4,8], # left diagonal win
+  [2,4,6], # right diagonal win
 ]
 
 def won?(board)
@@ -32,4 +32,7 @@ def won?(board)
     end
   }
   return false
+end
+
+def full?(board)
 end
