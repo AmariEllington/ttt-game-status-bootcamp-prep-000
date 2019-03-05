9398f3daaa83a1d1e1190b40071d7874ee2f2b0f
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-05 20:48:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,9 +57,5 @@ end
 def winner
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return "X"
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combo
-  else
-    return nil
 end
-end
+
